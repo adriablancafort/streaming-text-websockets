a552e60cdbc03ca9793b8ff2a06dbcf05be85386
Adria Blancafort <adriablancafort@gmail.com>
2024-06-16 00:06:42 +0200

    first commit

---
+++ server.py
@@ -0,0 +1,15 @@
+import asyncio
+import websockets
+
+text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+
+async def server(websocket, path):
+    for token in text.split():
+        await asyncio.sleep(0.1)
+        await websocket.send(token)
+
+
+start_server = websockets.serve(server, "0.0.0.0", 8765)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
